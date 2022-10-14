c5607ae6bfe9e118dd3764a0f02bf0637ef9897d
YihaoSu <yhsu@astro.ncu.edu.tw>
2022-10-14 15:25:11 +0800

    新增g0v零時小學校的報導

--- poster.py
+++ poster.py
@@ -6,7 +6,7 @@ import graphviz as graphviz
 page_title = '如何透過參與黑客松，與學生們一同協作設計出能解決教學問題的方案'
 st.set_page_config(page_title=page_title, layout='wide')
 st.title(page_title)
-st.success('蘇羿豪(Astrohackers in Taiwan社群)@2022 K-12天文教育論壇')
+st.success('[蘇羿豪](https://astrobackhacker.tw/)(Astrohackers in Taiwan社群)@[2022 K-12天文教育論壇](http://ps1tw.astro.ncu.edu.tw/ttss/index.php/2022-k-12-forum/)')
 
 qr = qrcode.QRCode()
 qr.add_data(
@@ -26,3 +26,6 @@ with st.expander('什麼是黑客松？'):
     graph.edge('提出構想', '討論與實做')
     st.graphviz_chart(graph)
     st.markdown('以上步驟取自主軸為公民科技的[g0v黑客松網頁](https://jothon.g0v.tw/)，參與者曾在該黑客松協作出[口罩地圖](https://g0v.hackmd.io/@kiang/mask-info)、[Cofacts 真的假的](https://cofacts.tw/)、[阿美族萌典](https://amis.moedict.tw/)等專案。')
+
+with st.expander('透過黑客松讓學生提案、回饋他們在教育上遇到的問題，並參與協作解決方案'):
+    st.markdown('"兒童出點子 專家學者解題...日本社企LITALICO的研究員表示「孩子是教育的使用者，教育卻是大人們在欠缺與孩子溝通的狀況下，擅自建立的制度」，因此與日本公民科技組織Code for Japan合作舉辦黑客松競賽，「由兒童出點子、專家學者解題」" ~ 摘錄自「[g0v零時小學校開跑！科技協作社群期待迸出教育新解方](https://flipedu.parenting.com.tw/article/005691)」這篇報導。:point_right:進入[g0v 零時小學校網站](https://sch001.g0v.tw/)。')
